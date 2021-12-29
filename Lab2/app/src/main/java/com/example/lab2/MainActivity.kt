025a1f603b03ac4957eb8a8842de684696567b46
Dima Tymoshok <DVTymoshok@gmail.com>
2021-12-29 18:09:29 +0200

    Added labs 1,2,3

---
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import com.example.lab2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val dataModel: DataModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        openFrag(InpFragment.newInstance(),R.id.inp_frag)
+        openFrag(OutpFragment.newInstance(),R.id.outp_frag)
+        dataModel.operCounter.observe(this as LifecycleOwner,{
+            binding.textView4.text=it
+        })
+    }
+    private fun openFrag(f: Fragment, idHolder: Int){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(idHolder, f)
+            .commit()
+    }
+}
